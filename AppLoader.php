5280dd2f3314fa2895448b49cb2ea8473813dcfa
Lalit Tanwar <lalit.tanwar07@gmail.com>
2017-06-25 17:10:06 +0530

    dev dependencies added

--- AppLoader.php
+++ AppLoader.php
@@ -15,22 +15,20 @@ namespace bootphp\rudrax {
         public static $RX_MODE_DEBUG = FALSE;
         public static $DISPLAY_ERROR = FALSE;
 
-        public function __construct()
+        public function invoke($dir)
         {
             if (static::$DISPLAY_ERROR) {
                 error_reporting(E_ALL & ~E_DEPRECATED);
+                \bootphp\fn::println("DISPLAYING ERROR");
             } else {
                 ini_set('display_errors', 0);
                 ini_set('error_reporting', 0);
                 error_reporting(0);
             }
-        }
 
-        public function invoke($dir)
-        {
-            require_once("RudraX.php");
+            require_once("lib/rudrax/boot/RudraX.php");
             try {
-                RudraX::invoke(array(
+                \RudraX::invoke(array(
                     'RX_MODE_DEBUG' => static::$RX_MODE_DEBUG,
                     'PROJECT_ROOT_DIR' => $dir . "/"
                 ));
